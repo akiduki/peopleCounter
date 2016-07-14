2b70216df34dcb1b33b17ec582d31437158c614e
ChengeLi <cl2840@nyu.edu>
2016-07-13 21:09:25 -0400

    switch of visualization

--- test_bgSubtraction.py
+++ test_bgSubtraction.py
@@ -5,10 +5,10 @@ import time
 import pdb
 import matplotlib.pyplot as plt
 
-
 # 191334-vv-1, 190645-vv-1
 # cap = cv2.VideoCapture('../../data/191334-vv-1.avi')
-cap = cv2.VideoCapture('../../data/192.168.31.138_01_20160706191100879.mp4')
+# cap = cv2.VideoCapture('../../data/192.168.31.138_01_20160706191100879.mp4')
+cap = cv2.VideoCapture('/Users/Chenge/Desktop/stereo_vision/peopleCounter/data/190645-vv-1.avi')
 
 ret, frame = cap.read()
 
@@ -29,23 +29,30 @@ video = cv2.VideoWriter('output_detection2.avi',CODE_TYPE,6,(output_width,output
 (Cx_old,Cy_old) = (0,0)
 
 centerList = []
-fig = plt.figure('vis')
-axL = plt.subplot(1,1,1)
-im  = plt.imshow(np.zeros([output_height,output_width,3]))
-plt.axis('off')
 peopleCount = 0
 peopleDirection = 0
 dots = []
 annos = []
 directionList = []
 frameInd = 0
+Visualize = False
+
+
+if Visualize:
+    fig = plt.figure('vis')
+    axL = plt.subplot(1,1,1)
+    im  = plt.imshow(np.zeros([output_height,output_width,3]))
+    plt.axis('off')
+
+
 while(cap.isOpened()):
     frameInd+=1
     ret, frame = cap.read()
     if ret == False:
         break
-
-    start = time.clock()
+    print frameInd
+    if frameInd ==70:
+        start = time.clock()
 
     frame = cv2.resize(frame, (output_width, output_height), interpolation = cv2.INTER_CUBIC)
     fgmask = fgbg.apply(frame)
@@ -56,10 +63,10 @@ while(cap.isOpened()):
     center = None
 
     """reference grid"""
-    output_height,output_width
-    cv2.line(frame, (0,output_height/4), (output_width,output_height/4), (255, 0, 255),1)
-    cv2.line(frame, (0,output_height/2), (output_width,output_height/2), (255, 0, 255),1)
-    cv2.line(frame, (0,3*output_height/4), (output_width,3*output_height/4), (255, 0, 255),1)
+    if Visualize:
+        cv2.line(frame, (0,output_height/4), (output_width,output_height/4), (255, 0, 255),1)
+        cv2.line(frame, (0,output_height/2), (output_width,output_height/2), (255, 0, 255),1)
+        cv2.line(frame, (0,3*output_height/4), (output_width,3*output_height/4), (255, 0, 255),1)
 
     cntCount=0
     for cnt in contours:
@@ -82,10 +89,11 @@ while(cap.isOpened()):
         #     cv2.line(frame, center, (Cx_old,Cy_old), (0, 255, 0), 1)
         # (Cx_old,Cy_old) = center
 
-        if len(centerList)>0:
-            lines = axL.plot(np.array(centerList)[:,0],np.array(centerList)[:,1],'-o',color = np.array([0, 1, 0]), linewidth=1)
-            line_exist = 1
-            # dots.append(axL.scatter(np.array(centerList)[:,0],np.array(centerList)[:,1]))
+        if Visualize:
+            if len(centerList)>0:
+                lines = axL.plot(np.array(centerList)[:,0],np.array(centerList)[:,1],'-o',color = np.array([0, 1, 0]), linewidth=1)
+                line_exist = 1
+                # dots.append(axL.scatter(np.array(centerList)[:,0],np.array(centerList)[:,1]))
 
     if len(centerList)>2:
         if (np.min(np.array(centerList)[:,1])< output_height/4) and (np.max(np.array(centerList)[:,1])>3*output_height/4):
@@ -110,25 +118,28 @@ while(cap.isOpened()):
             annos.append(plt.annotate('upward = '+str(np.sum(np.array(directionList)==2)), xy=(0.06*output_width,0.9*output_height), color='#ee8d18'))
             annos.append(plt.annotate('downward = '+str(np.sum(np.array(directionList)==1)), xy=(0.06*output_width,0.95*output_height),color='#ee8d18'))
             fig.canvas.draw()
+            end = time.clock()
+            print('detection time: {}'.format(end - start))
 
-    fname = '../frames/'+str(frameInd).zfill(6)+'.jpg'
+
+    # fname = '../frames/'+str(frameInd).zfill(6)+'.jpg'
     # plt.savefig(fname)
-    
-    im.set_data(frame[:,:,::-1])
-    fig.canvas.draw()
-    plt.show()
-    try:
-        axL.lines.pop(0)
-    except:
-        line_exist = 0
+    if Visualize:
+        im.set_data(frame[:,:,::-1])
+        fig.canvas.draw()
+        plt.show()
+        try:
+            axL.lines.pop(0)
+        except:
+            line_exist = 0
 
 
     # Detect blobs.
-    keypoints = detector.detect(fgmask)
-    print len(keypoints)
-    fgmask_keypoint = cv2.drawKeypoints(fgmask, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-    cv2.imshow('fgmask',fgmask)
-    end = time.clock()
+    # keypoints = detector.detect(fgmask)
+    # print len(keypoints)
+    # fgmask_keypoint = cv2.drawKeypoints(fgmask, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    # cv2.imshow('fgmask',fgmask)
+    # end = time.clock()
     # print('fps: {}'.format(1 / (end - start)))
 
     # cv2.imshow('frame',frame)
